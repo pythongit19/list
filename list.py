dd5fd3a8cd19735c0fb921d2824cfb778ff8c298
shima98 <pytorch.python19@gmail.com>
2022-07-27 17:03:13 +0430

    add list

---
+++ list.py
@@ -0,0 +1,17 @@
+colors = ["red", "blue", "green", "white"]
+print(len(colors))
+print(colors[0])
+colors[3] = 'yellow' 
+print(colors)
+colors.append('brown')
+print(colors)
+colors.remove('red')
+print(colors)
+colors.pop(2)
+print(colors)
+colors.pop()
+print(colors)
+del colors[0]
+print(colors)
+colors.clear()
+print(colors)
